e215012a37c597928583f69dd28f4d74d37f6981
Sergey L <lomgrey.s@gmail.com>
2021-05-15 12:17:19 +0300

    Change view

--- src/main/java/com/tasktimer/config/AppConfig.java
+++ src/main/java/com/tasktimer/config/AppConfig.java
@@ -2,7 +2,7 @@ package com.tasktimer.config;
 
 public final class AppConfig {
     public static final int INIT_ROOT_WIDTH = 490;
-    public static final int INIT_ROOT_HEIGHT = 260;
+    public static final int INIT_ROOT_HEIGHT = 230;
 
     public static final String APP_NAME = "Task Timer";
 
