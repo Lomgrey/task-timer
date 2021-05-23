920b9b40185456b99d05b1f00837ecb26463b6d8
Sergey L <lomgrey.s@gmail.com>
2021-05-23 21:16:47 +0300

    app icon

--- src/main/java/com/tasktimer/TaskTimerApp.java
+++ src/main/java/com/tasktimer/TaskTimerApp.java
@@ -5,13 +5,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 
 import static com.tasktimer.config.AppConfig.*;
+import static java.util.Objects.requireNonNull;
 
 public class TaskTimerApp extends Application {
 
@@ -28,6 +29,7 @@ public class TaskTimerApp extends Application {
         stage.setTitle(APP_NAME);
         stage.setMinHeight(MIN_ROOT_HEIGHT);
         stage.setMinWidth(MIN_ROOT_WIDTH);
+        addIcon(stage);
 
         Scene scene = new Scene(root, INIT_ROOT_WIDTH, INIT_ROOT_HEIGHT);
         scene.getStylesheets().add(getClass().getClassLoader().getResource("default.css").toExternalForm());
@@ -37,5 +39,10 @@ public class TaskTimerApp extends Application {
         controller.setStage(stage);
 
         stage.show();
+    }
+
+    private void addIcon(Stage stage) {
+        var iconResourceAsStream = getClass().getClassLoader().getResourceAsStream("app-icon.png");
+        stage.getIcons().add(new Image(requireNonNull(iconResourceAsStream)));
     }
 }
